302ecc2f998521d0a148f422c5bec429527a98d8
marcobergman <marcobergman@gmail.com>
2021-02-06 13:36:57 +0100

    added change dir and comment

--- refresh_browser.sh
+++ refresh_browser.sh
@@ -1,3 +1,5 @@
+# put this script in your home directory. You need to run it after each signalk server update.
+cd /usr/lib/node_modules/signalk-server/public/
 sudo rm -Rf SignalkDataBrowser/
 sudo git clone https://github.com/marcobergman/SignalkDataBrowser/
 sudo ln -s SignalkDataBrowser/SignalkDataBrowser.html
