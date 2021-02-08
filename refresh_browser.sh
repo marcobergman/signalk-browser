701ea7977db03a98d41540325b5c6469f7ed3624
Marco Bergman <marcobergman@gmail.com>
2021-02-08 17:50:08 +0100

    renamed to index.html

--- refresh_browser.sh
+++ refresh_browser.sh
@@ -6,4 +6,4 @@
 cd /usr/lib/node_modules/signalk-server/public/
 sudo rm -Rf SignalkDataBrowser/
 sudo git clone https://github.com/marcobergman/SignalkDataBrowser/
-sudo ln -s SignalkDataBrowser/SignalkDataBrowser.html
+sudo ln -s SignalkDataBrowser/index.html SignalkDataBrowser.html
